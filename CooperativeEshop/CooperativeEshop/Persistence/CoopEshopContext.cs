190fca0bfb3481ece730854b859447f4e7ec4c90
John Garos <33422873+JohnGaros@users.noreply.github.com>
2018-01-23 19:04:39 +0200

    IdentityUser initialization

---
+++ CooperativeEshop/CooperativeEshop/Persistence/CoopEshopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CooperativeEshop.Core.Domain;
+
+namespace CooperativeEshop.Persistence
+{
+    public class CoopEshopContext : IdentityDbContext<AppUser>
+    {
+        public CoopEshopContext(DbContextOptions<CoopEshopContext> options) : base(options)
+        {
+                
+        }
+    }
+}
--- CooperativeEshop/CooperativeEshop/Startup.cs
+++ CooperativeEshop/CooperativeEshop/Startup.cs
@@ -6,29 +6,41 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CooperativeEshop.Persistence;
+using CooperativeEshop.Core.Domain;
 
 namespace CooperativeEshop
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
         {
+            Configuration = config;    
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CoopEshopContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<CoopEshopContext>()
+                .AddDefaultTokenProviders();
+            services.AddMvc();
+        }
+
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
