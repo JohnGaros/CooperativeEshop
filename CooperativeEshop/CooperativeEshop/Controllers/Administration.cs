5c9843b1e896f1b1e8a83ea764c248a6b4fbdfbc
John Garos <33422873+JohnGaros@users.noreply.github.com>
2018-01-30 10:31:56 +0200

    Add Administrator Panel and basic navigation links

---
+++ CooperativeEshop/CooperativeEshop/Controllers/Administration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativeEshop.Controllers
+{
+    public class Administration : Controller
+    {
+        public ViewResult Admin() => View();
+    }
+}
